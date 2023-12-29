4ed3fa5b6fa24fbfe68f35fe9a0f71c34d60d7ea
Fady Ashraf <106484095+fadyashraf89@users.noreply.github.com>
2023-12-29 03:26:52 +0200

    Add files via upload

--- Web Lab Task/portfolio.js
+++ Web Lab Task/portfolio.js
@@ -1,39 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const profilePicture = document.getElementById('profilePicture');
+$(document).ready(function () {
+    const profilePicture = $('#profilePicture');
     const minScale = 0.80;
 
-    window.addEventListener('scroll', function () {
-        const scaleFactor = Math.max(minScale, 1 - window.scrollY * 0.001);
+    $(window).scroll(function () {
+        const scaleFactor = Math.max(minScale, 1 - $(window).scrollTop() * 0.001);
+        profilePicture.css('transform', 'scale(' + scaleFactor + ')');
 
-        profilePicture.style.transform = `scale(${scaleFactor})`;
-
-        if (window.scrollY > 100) {
-            profilePicture.classList.add('small');
+        if ($(window).scrollTop() > 100) {
+            profilePicture.addClass('small');
         } else {
-            profilePicture.classList.remove('small');
+            profilePicture.removeClass('small');
         }
     });
-});
 
-$(document).ready(function () {
-    // Smooth scroll to anchor links
     $('.scroll-link').click(function (e) {
         e.preventDefault();
         var targetId = $(this).attr('href');
         $('html, body').animate({
-                scrollTop: $(targetId).offset().top - $('header').outerHeight()
-            },
-            {
-                duration: 100,
-            });
+            scrollTop: $(targetId).offset().top - $('header').outerHeight()
+        }, {
+            duration: 100,
+        });
     });
 
-    // Add scrolled class on scroll
     $(window).scroll(function () {
         if ($(this).scrollTop() > 0) {
             $('header').addClass('scrolled');
         } else {
             $('header').removeClass('scrolled');
         }
+    });
+
+    $("#profilePicture").click(function () {
+        $(this).toggleClass("transparent");
     });
 });
